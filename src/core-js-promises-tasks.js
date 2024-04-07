cfdc4a063ec89e45d2abea3279508ac871c446d0
Pavel <you@example.com>
2024-04-07 20:08:55 +0300

    feat: implement queuPromises task (07.04.24 20:08)

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -139,8 +139,22 @@ function getAllResult(promises) {
  * [promise1, promise4, promise3] => Promise.resolved('104030')
  * [promise1, promise4, promise3, promise2] => Promise.resolved('10403020')
  */
-function queuPromises(/* promises */) {
-  throw new Error('Not implemented');
+
+async function queuPromises(promises) {
+  let result = '';
+
+  function chainPromise(index) {
+    if (index >= promises.length) {
+      return Promise.resolve(result);
+    }
+
+    return promises[index].then((value) => {
+      result += value;
+      return chainPromise(index + 1);
+    });
+  }
+
+  return chainPromise(0);
 }
 
 module.exports = {
